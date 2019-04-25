db9de4ce1ea4729389e3e9cca8d8fd04bbd25513
daring-board <mahotox101500@gmail.com>
2019-04-25 21:46:24 +0900

    port番号変更

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ if __name__ == "__main__":
     t.daemon = True
     t.start()
     print(" * Flask starting server...")
-    app.run(host='0.0.0.0',port=5001)
+    app.run(host='0.0.0.0',port=80)
